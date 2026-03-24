42c1d2cb5d295696b952d156ef02fa6a8cc9b2e0
Wordluc <frangiamoreluca@gmail.com>
2026-03-24 16:53:46 +0100

    refactor: define common function to define is a cell is flammable

--- core/utils.go
+++ core/utils.go
@@ -3,6 +3,7 @@ package core
 import (
 	"math/rand"
 	"sand-mmo/cell"
+	"sand-mmo/common"
 )
 
 // NewCellByChance generates a cell with type "typeCell" with a probability of chance/10.
@@ -14,4 +15,19 @@ func NewCellByChance(typeCell cell.CellType, chance int) (cell.Cell, bool) {
 	}
 	c := cell.NewCell(cell.EMPTY_CELL)
 	return c, false
+}
+
+func (w *ServerWorld) isFree(pos common.Vec2) bool {
+	x, y := pos.Get()
+	c := w.Get(x, y)
+	return c != nil && c.IsEmpty()
+}
+
+func (w *ServerWorld) isFlammable(c *cell.Cell) bool {
+	return c.CellType == cell.WOOD_CELL || c.CellType == cell.LEAF_CELL
+}
+
+func (w *ServerWorld) setEmptyCell(pos common.Vec2) error {
+	w.SetVec(pos, cell.NewCell(cell.EMPTY_CELL))
+	return nil
 }
