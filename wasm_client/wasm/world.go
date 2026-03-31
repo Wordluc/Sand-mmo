d544bcf81c063176ed2200d79bef96596af0ba12
Wordluc <frangiamoreluca@gmail.com>
2026-03-31 14:44:07 +0200

    refacotor: use W,H of the world instead of common

--- wasm_client/wasm/world.go
+++ wasm_client/wasm/world.go
@@ -1,7 +1,6 @@
 package wasm
 
 import (
-	"sand-mmo/common"
 	"sand-mmo/core"
 	"syscall/js"
 )
@@ -10,8 +9,8 @@ func (state *WasmState) InitWorld() {
 	state.World = new(core.NewClientWorld())
 	doc := js.Global().Get("document")
 	div := doc.Call("getElementById", "GAME_WINDOW")
-	div.Set("width", SIZE_CELL*common.W_CELLS_CLIENT)
-	div.Set("height", SIZE_CELL*common.H_CELLS_CLIENT)
+	div.Set("width", SIZE_CELL*state.World.W)
+	div.Set("height", SIZE_CELL*state.World.H)
 	state.Document = doc
 	state.Ctx2D = div.Call("getContext", "2d")
 }
