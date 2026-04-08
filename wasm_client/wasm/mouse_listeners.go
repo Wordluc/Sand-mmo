1328105312c8ccb74901ee1b3425d8f56d12c301
Wordluc <frangiamoreluca@gmail.com>
2026-04-08 08:31:02 +0200

    fix: mouse and touch movement inside canvas

--- wasm_client/wasm/mouse_listeners.go
+++ wasm_client/wasm/mouse_listeners.go
@@ -3,27 +3,41 @@ package wasm
 import "syscall/js"
 
 func (state *WasmState) AddMouseEventListeners() {
-	rectDiv := state.Document.Call("getElementById", GAME_WINDOW).Call("getBoundingClientRect")
+	canvas := state.Document.Call("getElementById", GAME_WINDOW)
+	rectDiv := canvas.Call("getBoundingClientRect")
 	xStart, yStart := rectDiv.Get("x").Int(), rectDiv.Get("y").Int()
+	width := canvas.Get("offsetWidth").Int()
+	height := canvas.Get("offsetHeight").Int()
 
-	state.Document.Call("addEventListener", "mousemove", js.FuncOf(func(this js.Value, args []js.Value) any {
+	canvas.Call("addEventListener", "mousemove", js.FuncOf(func(this js.Value, args []js.Value) any {
 		state.Mouse.Set(args[0].Get("clientX").Int()-xStart, args[0].Get("clientY").Int()-yStart)
 		return nil
 	}))
-	state.Document.Call("addEventListener", "mousedown", js.FuncOf(func(this js.Value, args []js.Value) any {
+	canvas.Call("addEventListener", "mousedown", js.FuncOf(func(this js.Value, args []js.Value) any {
 		state.Mouse.Pressed = true
 		return nil
 	}))
-	state.Document.Call("addEventListener", "mouseup", js.FuncOf(func(this js.Value, args []js.Value) any {
+	canvas.Call("addEventListener", "mouseleave", js.FuncOf(func(this js.Value, args []js.Value) any {
 		state.Mouse.Pressed = false
 		return nil
 	}))
-	state.Document.Call("addEventListener", "touchmove", js.FuncOf(func(this js.Value, args []js.Value) any {
-		touch := args[0].Get("touches").Call("item", 0)
-		state.Mouse.Set(touch.Get("clientX").Int()-xStart, touch.Get("clientY").Int()-yStart)
+	canvas.Call("addEventListener", "mouseup", js.FuncOf(func(this js.Value, args []js.Value) any {
+		state.Mouse.Pressed = false
 		return nil
 	}))
-	state.Document.Call("addEventListener", "touchstart", js.FuncOf(func(this js.Value, args []js.Value) any {
+	canvas.Call("addEventListener", "touchmove", js.FuncOf(func(this js.Value, args []js.Value) any {
+		touch := args[0].Get("touches").Index(0)
+		x := touch.Get("clientX").Int() - xStart
+		y := touch.Get("clientY").Int() - yStart
+
+		if x < 0 || y < 0 || x > width || y > height {
+			state.Mouse.Pressed = false
+		}
+
+		state.Mouse.Set(x, y)
+		return nil
+	}))
+	canvas.Call("addEventListener", "touchstart", js.FuncOf(func(this js.Value, args []js.Value) any {
 		touch := args[0].Get("touches").Call("item", 0)
 		state.Mouse.Set(touch.Get("clientX").Int()-xStart, touch.Get("clientY").Int()-yStart)
 		state.Mouse.Pressed = true
@@ -33,6 +47,6 @@ func (state *WasmState) AddMouseEventListeners() {
 		state.Mouse.Pressed = false
 		return nil
 	})
-	state.Document.Call("addEventListener", "touchend", releaseFn)
-	state.Document.Call("addEventListener", "touchcancel", releaseFn)
+	canvas.Call("addEventListener", "touchend", releaseFn)
+	canvas.Call("addEventListener", "touchcancel", releaseFn)
 }
