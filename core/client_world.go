9f9681bcb9bb336315bf56f9d61177871f942f78
Wordluc <frangiamoreluca@gmail.com>
2026-04-19 15:45:04 +0200

    fix: use relative w_chunk to set decoded cells

--- core/client_world.go
+++ core/client_world.go
@@ -11,6 +11,8 @@ type ClientWorld struct {
 	right_chunks []Cell
 	up_chunks    []Cell
 	down_chunks  []Cell
+	w_chunks     int
+	h_chunks     int
 }
 
 func NewCustomWorld(w, h, chunkSize int) (res ClientWorld) {
@@ -19,6 +21,8 @@ func NewCustomWorld(w, h, chunkSize int) (res ClientWorld) {
 	res.right_chunks = make([]Cell, h*chunkSize*chunkSize)
 	res.up_chunks = make([]Cell, w*chunkSize*chunkSize)
 	res.down_chunks = make([]Cell, w*chunkSize*chunkSize)
+	res.w_chunks = w / chunkSize
+	res.h_chunks = h / chunkSize
 	return res
 }
 
@@ -76,11 +80,13 @@ func (w *ClientWorld) SetDecodedCells(bytes []byte, xChunk, yChunk int) {
 	iCell := 0
 	iBorder := 0
 	isXOut := func(xChunk int) bool {
-		return xChunk < 0 || xChunk >= common.W_CHUNKS_CLIENT
+		return xChunk < 0 || xChunk >= w.w_chunks
 	}
+
 	isYOut := func(yChunk int) bool {
-		return yChunk < 0 || yChunk >= common.H_CHUNKS_CLIENT
+		return yChunk < 0 || yChunk >= w.h_chunks
 	}
+
 	for i := 0; i < len(bytes); i = i + u32Size {
 		u16 = binary.BigEndian.Uint16(bytes[i : i+u32Size])
 		c = DecodeCell(u16)
@@ -124,7 +130,7 @@ func (w *ClientWorld) SetDecodedCells(bytes []byte, xChunk, yChunk int) {
 			iCell += (w.W - w.ChunkSize)
 		}
 	}
-	w.activeChunks.SortedInsert(xChunk + yChunk*common.W_CHUNKS_CLIENT)
+	w.activeChunks.SortedInsert(xChunk + yChunk*w.w_chunks)
 }
 
 func (w *ClientWorld) PopActiveChunks() (res []int) {
